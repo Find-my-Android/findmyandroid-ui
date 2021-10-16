4c1f13bc1afc812382ffcc210b52c4b2a82f2548
May <giftedliu12@gmail.com>
2021-10-16 15:22:55 -0500

    account front-end (only how looks like)

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -4,8 +4,12 @@ import SideBar from "../components/SideBar";
 import { useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
 
+import "../styles/account.css"
+
 function Account({ history }) {
   const authenticated = useSelector((state) => state.authenticated);
+  const user = useSelector((state) => state.user);
+
 
   useEffect(() => {
     if (!authenticated) {
@@ -16,7 +20,24 @@ function Account({ history }) {
   return (
     <>
       <SideBar history={history} active="account" />
-    </>
+      <div className="a">
+        <div className = "a1">
+        {user.first_name} {user.last_name}
+          <div className="a2">
+            Name:  {user.first_name} {user.last_name}
+          </div>
+          <p className="change">Change Name</p>
+          <div className="a2">
+            Email: jsmith@gmail.com
+          </div>
+          <p className="change">Change email</p>
+          <div className="a2">
+            Password: *************
+          </div>
+          <p className="change">Change password</p>
+        </div>
+      </div>
+      </>
   );
 }
 
